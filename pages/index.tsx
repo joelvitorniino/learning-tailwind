a4bb59bf686ab2f2c716196d97aa37362b5f45ab
joelvitorniino <joelprofessor13@gmail.com>
2022-12-18 16:10:08 -0300

    border

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,14 @@
 export default function Home() {
   return (
     <>
-      <div className="p-4 m-4">
-      Lorem ipsum dolor sit amet. Qui recusandae fugit id soluta molestiae et libero distinctio eos eveniet provident? Ut recusandae nobis et consequatur maiores vel dicta velit. Ea nostrum expedita ea sapiente omnis aut iusto praesentium.
+      <div className="m-5 p-4 border-l-2 border-yellow-400">
+        Lorem ipsum dolor sit amet. Qui recusandae fugit id soluta molestiae et libero distinctio eos eveniet provident? Ut recusandae nobis et consequatur maiores vel dicta velit. Ea nostrum expedita ea sapiente omnis aut iusto praesentium.
       </div>
-    
-      <div className="m-4 space-x-4">
-        <span>1</span>
-        <span>2</span>
-        <span>3</span>
+
+      <div className="m-5 p-4 border border-red-400 rounded border-opacity-20">
+        Lorem ipsum dolor sit amet. Qui recusandae fugit id soluta molestiae et libero distinctio eos eveniet provident? Ut recusandae nobis et consequatur maiores vel dicta velit. Ea nostrum expedita ea sapiente omnis aut iusto praesentium.
+        <br />
+        <button className="bg-blue-400 px-4 py-1 rounded text-white hover:ring-2">Save</button>
       </div>
     </>
   )
