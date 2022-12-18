28ef83d7080bca737d1c12e9db6696df76b02c44
joelvitorniino <joelprofessor13@gmail.com>
2022-12-18 15:55:48 -0300

    padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,19 +1,15 @@
 export default function Home() {
   return (
     <>
-    <h1 className="text-3x1 font-bold underline">
-        Hello world!
-    </h1>
-
-    <div className='p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4'>
-      <div className="shrink-0">
-        <img src="/img/icon.svg" alt="Chat TailWind" className="h12 w-12" />
+      <div className="p-4 m-4">
+      Lorem ipsum dolor sit amet. Qui recusandae fugit id soluta molestiae et libero distinctio eos eveniet provident? Ut recusandae nobis et consequatur maiores vel dicta velit. Ea nostrum expedita ea sapiente omnis aut iusto praesentium.
       </div>
-      <div>
-        <div className="text-xl font-medium text-black">TailChat</div>
-        <p className="text-slate-500">You have a new message!</p>
+    
+      <div className="m-4 space-x-4">
+        <span>1</span>
+        <span>2</span>
+        <span>3</span>
       </div>
-    </div>
     </>
   )
 }
