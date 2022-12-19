3acac99824c278bc3f1f53d4dff321956915eab6
joelvitorniino <joelprofessor13@gmail.com>
2022-12-19 13:53:51 -0300

    width and height

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,8 @@
 export default function Home() {
   return (
     <>
-     <div className="bg-gray-100">
-      <div className="n5 p-4 text-white bg-blue-900 bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500">
-        Lorem ipsum dolor sit amet. Qui recusandae fugit id soluta molestiae et libero distinctio eos eveniet provident? Ut recusandae nobis et consequatur maiores vel dicta velit. Ea nostrum expedita ea sapiente omnis aut iusto praesentium.
-      </div>
-
-      <div className="n5 p-4 text-white bg-blue-900 bg-gradient-to-bl from-gray-900 via-blue-500 to-blue-200">
+     <div className="bg-gray-100 h-screen">
+      <div className="max-w-md h-screen p-4 text-white bg-blue-900 bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500">
         Lorem ipsum dolor sit amet. Qui recusandae fugit id soluta molestiae et libero distinctio eos eveniet provident? Ut recusandae nobis et consequatur maiores vel dicta velit. Ea nostrum expedita ea sapiente omnis aut iusto praesentium.
       </div>
      </div>
