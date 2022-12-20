26e085f3f085e6862738621220ae294caa9378ec
joelvitorniino <joelprofessor13@gmail.com>
2022-12-20 11:03:12 -0300

    flexbo x

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,20 @@
+"        Lorem ipsum dolor sit amet. Qui recusandae fugit id soluta molestiae et libero distinctio eos eveniet provident? Ut recusandae nobis et consequatur maiores vel dicta velit. Ea nostrum expedita ea sapiente omnis aut iusto praesentium."
+
 export default function Home() {
   return (
     <>
      <div className="bg-gray-100 h-screen">
-      <div className="max-w-md h-screen p-4 text-white bg-blue-900 bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500">
-        Lorem ipsum dolor sit amet. Qui recusandae fugit id soluta molestiae et libero distinctio eos eveniet provident? Ut recusandae nobis et consequatur maiores vel dicta velit. Ea nostrum expedita ea sapiente omnis aut iusto praesentium.
+      <div className="flex justify-center items-center space-x-4 bg-gray-300 h-36">
+        <div className="w-24 h-24 p-4 text-white bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500"></div>
+        <div className="w-24 h-24 p-4 text-white bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500"></div>
+        <div className="w-24 h-24 p-4 text-white bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500"></div>
+      </div>
+      <br />
+
+      <div className="flex justify-center items-center space-x-4 bg-gray-300 h-36">
+        <div className="w-24 h-24 p-4 text-white bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500"></div>
+        <div className="flex-grow flex-shrink-0 w-24 h-24 p-4 text-white bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500"></div>
+        <div className="w-24 h-24 p-4 text-white bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500"></div>
       </div>
      </div>
     </>
