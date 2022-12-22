52b3978ca715f33112d441e22882668527a9b6a8
joelvitorniino <joelprofessor13@gmail.com>
2022-12-22 13:24:05 -0300

    responsive

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,11 @@
-"        Lorem ipsum dolor sit amet. Qui recusandae fugit id soluta molestiae et libero distinctio eos eveniet provident? Ut recusandae nobis et consequatur maiores vel dicta velit. Ea nostrum expedita ea sapiente omnis aut iusto praesentium."
-
 export default function Home() {
   return (
     <>
-     <div className="bg-gray-100 h-screen">
-      <div className="flex justify-center items-center space-x-4 bg-gray-300 h-36">
-        <div className="w-24 h-24 p-4 text-white bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500"></div>
-        <div className="w-24 h-24 p-4 text-white bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500"></div>
-        <div className="w-24 h-24 p-4 text-white bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500"></div>
-      </div>
-      <br />
+      <div className="bg-gray-300 sm:bg-red-300 h-screen">
+        <h1 className="text-sm sm:text-base md:text-xl lg:text-2xl">Hello TailwindCSS</h1>
 
-      <div className="flex justify-center items-center space-x-4 bg-gray-300 h-36">
-        <div className="w-24 h-24 p-4 text-white bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500"></div>
-        <div className="flex-grow flex-shrink-0 w-24 h-24 p-4 text-white bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500"></div>
-        <div className="w-24 h-24 p-4 text-white bg-gradient-to-bl from-yellow-400 via-red-500 to-pink-500"></div>
+        <div className=""></div>
       </div>
-     </div>
     </>
   )
 }
