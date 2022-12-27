9ae5ecc60dbd9b6fabab2c7ec5a00924d8188ae7
joelvitorniino <joelprofessor13@gmail.com>
2022-12-27 13:47:42 -0300

    component button

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,19 +2,32 @@
 export default function Home() {
   return (
     <>
-      <div className="bg-gray-50 container mx-auto px-4 py-8 dark:bg-gray-800">
-        <h6 className="dark:text-gray-50 font-bold mb-2">Componente Card</h6>
+      <div className="bg-gray-50 container mx-auto px-4 py-8 space-y-8">
+      <div>
+          <h6 className="font-bold mb-2">Componente Button</h6>
+          <button className="text-sm bg-blue-500 hover:bg-blue-600 px-4 py-1 rounded text-white shadow">
+            Save
+          </button>
+          <button className="text-sm bg-blue-500 hover:bg-blue-600 px-6 py-1 rounded text-white shadow">
+            Save
+          </button>
+          <button className="text-base bg-blue-500 hover:bg-blue-600 px-8 py-3 rounded text-white shadow">
+            Save
+          </button>
+          <hr />
+      </div>
+        <h6 className="font-bold mb-2">Componente Card</h6> 
         
-        <div className="bg-white dark:bg-gray-700 rounded-lg shadow-md overflow-hidden">
-          <div className="w-full bg-gray-100 dark:bg-gray-900 p-4 border-b border-gray-200 dark:border-gray-500">
-            <h4 className="font-bold dark:text-gray-50">Topo</h4>
+        <div className="bg-white rounded-lg shadow-md overflow-hidden">
+          <div className="w-full bg-gray-100 p-4 border-b border-gray-200">
+            <h4 className="font-bold">Topo</h4>
           </div>
 
-          <div className="p-4 dark:text-gray-50">
+          <div className="p-4">
             Lorem ipsum dolor sit amet, constectur adipiscing elit. Alias
           </div>
 
-          <div className="w-full dark:text-gray-50 bg-gray-50 dark:bg-gray-500 p-3 border-t border-gray-100 dark:border-gray-500">
+          <div className="w-full dark:text-gray-50 bg-gray-50 p-3 border-t border-gray-100">
             Rodapé
           </div>
         </div>
